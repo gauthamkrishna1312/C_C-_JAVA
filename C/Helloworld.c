3a840628226b9ba1ace3eab5b8e55a6afb002133
Gautham Krishna P <gauthamkrishna1312@gmail.com>
2020-07-30 07:34:50 +0530

    FirstCommit

---
+++ C/Helloworld.c
@@ -0,0 +1,6 @@
+#include<stdlib.h>
+#include<stdio.h>
+int main(void){
+    printf("Hellow World");
+    printf("\n");
+}
---
+++ C/input1.c
@@ -0,0 +1,9 @@
+#include<stdlib.h>
+#include<stdio.h>
+int main(void){
+    int dig1;
+    printf("Enter a digit");
+    scanf("%d",&dig1);
+    printf("You are entered %d",dig1);
+    return EXIT_SUCCESS;
+}
